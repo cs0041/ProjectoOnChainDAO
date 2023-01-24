d6db2574bf2f5a4ec693e5415f29fcb6e307e0f7
cs0041 <pamzalove20@hotmail.com>
2023-01-24 23:29:01 +0700

    Update 01-deploy-governor-token.ts

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,27 +1,29 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
+  // @ts-ignore
 import { ethers } from 'hardhat';
 
 const func: DeployFunction = async function (
     hre: HardhatRuntimeEnvironment
     ) {
-        const {getNamedAccounts,deployments,network} = hre
-        const {deploy , log} = deployments
-        const { deployer } = await getNamedAccounts()
-        log('----------------------------------------------------')
-        log('Deploying GovernanceToken and waiting for confirmations...')
-        const governanceToken = await deploy('GovernanceToken', {
-          from: deployer,
-          args: [],
-          log: true,
-          // we need to wait if on a live network so we can verify properly
+      // @ts-ignore
+      const { getNamedAccounts, deployments, network } = hre
+      const { deploy, log } = deployments
+      const { deployer } = await getNamedAccounts()
+      log('----------------------------------------------------')
+      log('Deploying GovernanceToken ')
+      const governanceToken = await deploy('GovernanceToken', {
+        from: deployer,
+        args: [],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
         //   waitConfirmations:
         //     networkConfig[network.name].blockConfirmations || 1,
-        })
-         log(`GovernanceToken at ${governanceToken.address}`)
-        
-          await delegate(governanceToken.address, deployer)
-          log('Delegated!')
+      })
+      log(`GovernanceToken at ${governanceToken.address}`)
+
+      await delegate(governanceToken.address, deployer)
+      log('Delegated!')
     };
 
     const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
